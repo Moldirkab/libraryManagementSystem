f0d9e4e5697431282dd726aebfdd1d4b40260acb
rauan017 <dayletali84@gmail.com>
2025-01-29 21:28:47 +0500

    Delete ITransactionsController.java

--- ITransactionsController.java
+++
@@ -1,6 +0,0 @@
-package controllers;
-
-public interface ITransactionsController {
-    String borrowBook(int member_id, int book_id,int[] currentDate);
-    String returnBook(int member_id, int book_id,int[] currentDate);
-}
