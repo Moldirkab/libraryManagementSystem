58a718f74b0e859dc0f967c1706761db38c696fc
rauan017 <dayletali84@gmail.com>
2025-01-29 21:30:33 +0500

    Delete IReaderController.java

--- IReaderController.java
+++
@@ -1,8 +0,0 @@
-package controllers;
-
-import models.Reader;
-
-public interface IReaderController {
-    Reader findReaderByIdPassword(int searchId, String password);
-    void addReader(Reader newReader);
-}
